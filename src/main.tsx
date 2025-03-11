3b0226238588bfc6903908a82861bf0a0dc70a62
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2025-03-11 18:12:30 +0800

    fix: missing css

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import "@rainbow-me/rainbowkit/styles.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
