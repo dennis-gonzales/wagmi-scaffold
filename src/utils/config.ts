b6bbe3895edd1efa2f537e93ba14676cf73a2c54
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2025-03-11 18:12:38 +0800

    refactor: config ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,15 +1,38 @@
 import { http } from "wagmi";
-import { bsc, bscTestnet, mainnet } from "wagmi/chains";
+import {
+  arbitrum,
+  arbitrumGoerli,
+  bsc,
+  bscTestnet,
+  mainnet as ethereum,
+  polygon,
+  polygonMumbai,
+  sepolia,
+} from "wagmi/chains";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
+
+const isProduction = false;
 
 export const config = getDefaultConfig({
   appName: "My RainbowKit App",
   projectId: "YOUR_PROJECT_ID",
-  chains: [mainnet, bsc, bscTestnet],
+  chains: isProduction
+    ? [ethereum, bsc, polygon, arbitrum]
+    : [sepolia, bscTestnet, polygonMumbai, arbitrumGoerli],
   transports: {
-    [mainnet.id]: http(),
+    [ethereum.id]: http(),
+    [sepolia.id]: http(),
     [bsc.id]: http(),
     [bscTestnet.id]: http(),
+    [polygon.id]: http(),
+    [polygonMumbai.id]: http(),
+    [arbitrum.id]: http(),
+    [arbitrumGoerli.id]: http(),
   },
-  // ssr: true,
 });
